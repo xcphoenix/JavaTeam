d7cdaed45ce063a1e652a363d545f35fe6024d80
PhoenixXC <bmingyy@qq.com>
2019-08-04 23:18:45 +0800

    fix

--- zxx/web-pratice-demo/SpringBoot/auth/auth-jwt-01/src/main/java/com/xuanc/authjwt/config/InterceptorConfig.java
+++ zxx/web-pratice-demo/SpringBoot/auth/auth-jwt-01/src/main/java/com/xuanc/authjwt/config/InterceptorConfig.java
@@ -14,6 +14,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
 
+    /**
+     * TODO 拦截器中 bean 会出现 bean 无法注入，值为 null 的情况
+     **/
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor())
